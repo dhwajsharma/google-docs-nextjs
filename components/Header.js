08ad8cbf9e499181dc190aa14f4f9902350a370b
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-07-15 10:57:45 +0530

    Built section header

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Icon from "@material-tailwind/react/Icon"
 
 const Header = () => {
     return (
-        <div className="sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white">
+        <header className="sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white">
             <Button
                 color="gray"
                 buttonType="outline"
@@ -41,7 +41,7 @@ const Header = () => {
                 alt=""
             />
 
-        </div>
+        </header>
     )
 }
 
