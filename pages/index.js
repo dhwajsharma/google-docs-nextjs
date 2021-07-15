7b49ca5f9f24f771dda831af7a87a19d14be78f9
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-07-15 21:11:16 +0530

    built section below section header

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Button from '@material-tailwind/react/Button'
 import Icon from '@material-tailwind/react/Icon'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Image from "next/image"
 
 export default function Home() {
   return (
@@ -27,9 +28,24 @@ export default function Home() {
               <Icon name="more_vert" size="3xl" />
             </Button>
           </div>
+          <div>
+            <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700">
+              <Image src="https://links.papareact.com/pju" layout="fill" />
+            </div>
+            <p className="ml-2 mt-2 font-semibold text-sm text-gray-700">Blank</p>
+          </div>
         </div>
       </section>
 
+      <section className="bg-white px-10 md:px-0 ">
+        <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
+          <div className="flex items-center justify-between pb-5">
+            <h2 className="font-medium flex-grow">My Documents</h2>
+            <p className="mr-12">Date Created</p>
+            <Icon name="folder" size="3xl" color="gray" />
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
